358e15186492bab98b682d19f71a7304eb320828
ali1351939 <aliashrafizadeh@yahoo.com>
2023-10-25 12:19:37 -0400

    updates

--- app.js
+++ app.js
@@ -416,7 +416,7 @@ app.post('/converter', async (req, res) => {
     }
   });
   
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 3002;
   app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
   });
